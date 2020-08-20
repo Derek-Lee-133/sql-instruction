b55e56866dc8cf1d866ba5ae2fc6288c9bf081e8
Derek Lee <dlee133@gmail.com>
2020-08-20 17:11:59 -0400

    sql-insert-08-20-2020

---
+++ bmdb/sql-inserts-bmdb-08-20-2020.sql
@@ -0,0 +1,26 @@
+SELECT * FROM movie;
+insert into movie
+	values 
+            (17, 'Knotting Hill', '1999', 'PG-13', 'Roger Michell'),
+            (18, 'Brotherhood of the Wolf', '2001', 'R', 'Christophe Gans'),
+            (19, 'The Crow', '1994', 'R', 'Alex Proyas'),
+            (20, 'The Terminater', '1984', 'R', 'James Cameron');
+ 
+ 
+ select * from actor;
+ insert into actor 
+	(firstname, lastname, gender, birthdate)
+    values
+
+    ('Norman', 'Reedus', 'M', '1969-01-06');
+    
+    select * from credit;
+    
+    insert into credit 
+    ( actorid, movieid, role) values
+    
+    ( 5, 19, 'Eric');
+            
+            
+select * from actor
+where firstname = 'brandon';
