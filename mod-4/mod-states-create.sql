0aca6af42646a2aee44d2a774a1c72460f57bd7b
Derek Lee <dlee133@gmail.com>
2020-08-22 13:51:27 -0400

    school

---
+++ mod-4/mod-states-create.sql
@@ -0,0 +1,4 @@
+create table state ( 
+code nvarchar(2) not null primary key,
+name nvarchar(50) not null);
+
---
+++ mod-4/mod4-state-table.sql
@@ -0,0 +1,13 @@
+SELECT * FROM sqlbootcampexample.state;
+
+SELECT * FROM sqlbootcampexample.state;
+
+insert state values ('AL', 'Alabama');
+insert state values ('CA', 'California');
+ insert state values('HI', 'Hawaii');
+ insert state values ('NJ', 'New Jersey');
+ insert state values ('NV', 'Nevada');
+ insert state values ('OH', 'Ohio');
+ insert state values ('TX', 'Texas');
+ insert state values ('WA', 'Washington');
+ 
